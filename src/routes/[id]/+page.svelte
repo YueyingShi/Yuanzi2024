c6e92e99543aa8ce4a02bbb256d096963fa05451
YueyingShi <sharonstone0524@gmail.com>
2024-10-18 12:11:03 +0200

    that would be ite for now

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -34,18 +34,14 @@
 	<div class="w-full flex">
 		<button on:click={goBack} class=" flex items-center gap-2 text-gray-800">
 			<Icon src={ArrowLeft} size="20" solid />
-			<p class="text-lg text-gray-700">Chapters/ Chapter{id}</p>
+			<p class=" text-gray-700">Chapter{id}</p>
 		</button>
 	</div>
 
 	<!-- chapter information -->
 	<div class="flex flex-col w-full h-full items-center justify-center gap-4">
 		<!-- Avatar image -->
-		<img
-			src="/avatars/chapter{id}.png"
-			alt="Chapter {id} avatar"
-			class="w-40 h-40 rounded-full mb-4 border border-gray-300 mt-2"
-		/>
+		<img src="/avatars/chapter{id}.png" alt="Chapter {id} avatar" class="w-56 h-56 mb-4 mt-2" />
 		<div class="flex flex-col w-full gap-1 mb-8">
 			<h1 class="text-xl font-medium">{title}</h1>
 			<h2 class="text-xl text-gray-800">{subtitle}</h2>
