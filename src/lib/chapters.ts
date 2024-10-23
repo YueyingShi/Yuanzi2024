7ad70c2b5be261816773726bc47fea930c137806
Ellan <sharonstone0524@gmail.com>
2024-10-23 17:16:45 +0200

    More title

--- src/lib/chapters.ts
+++ src/lib/chapters.ts
@@ -1,12 +1,12 @@
 export const chapters = [
-	{ story: 'palace', title: 'In the Palace', subtitle: '' },
-	{ story: 'trainers', title: 'Tower and the Trainers', subtitle: '' },
-	{ story: 'animals', title: 'Animals in the Wild', subtitle: 'Ruirui, Jiejie & 熊熊 说:' },
-	{ story: 'robot', title: 'Awaken', subtitle: '' },
-	{ story: 'drivers', title: 'Drivers', subtitle: 'Yumi, Xie & David 说:' },
-	{ story: 'robot2', title: 'The Rude Robot', subtitle: '' },
-	{ story: 'robot3', title: "Princess' New Speaker", subtitle: '' },
-	{ story: 'dragon1', title: 'Face Your Fear', subtitle: '' },
-	{ story: 'dragon2', title: 'Victory', subtitle: '' }
+	{ story: 'palace', title: 'In the Palace', subtitle: 'Mya & Peter said,' },
+	{ story: 'trainers', title: 'Tower and the Trainers', subtitle: 'Tian, Qiqi & Yoja said,' },
+	{ story: 'animals', title: 'Animals in the Wild', subtitle: 'Ruirui, Jiejie & 熊熊 said,' },
+	{ story: 'robot', title: 'Awaken', subtitle: 'Robot said,' },
+	{ story: 'drivers', title: 'Drivers', subtitle: 'Yumi, Xie & David said,' },
+	{ story: 'robot2', title: 'The Rude Robot', subtitle: 'Robot said,' },
+	{ story: 'robot3', title: "Princess' New Speaker", subtitle: 'Robot said,' },
+	{ story: 'dragon1', title: 'Face Your Fear', subtitle: 'Dragon said,' },
+	{ story: 'dragon2', title: 'Victory', subtitle: 'Dragon said,' }
 	// Add more chapters as needed
 ];
