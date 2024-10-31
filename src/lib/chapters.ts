d2d5c9b46d2fab3f080a206802ce2533b8d248a7
Ellan <sharonstone0524@gmail.com>
2024-10-31 14:58:57 +0100

    audios

--- src/lib/chapters.ts
+++ src/lib/chapters.ts
@@ -9,26 +9,41 @@ export const chapters = [
 		story: 'trainers',
 		title: 'Tower and the Trainers',
 		subtitle: 'Tian, Qiqi & Yoja said,',
-		audios: ['tian', 'qiqi', 'yoja']
+		audios: ['brother tian', 'qiqi', 'yoja']
 	},
 	{
 		story: 'animals',
 		title: 'Animals in the Wild',
 		subtitle: 'Ruirui, Jiejie & 熊熊 said,',
-		audios: ['ruirui', 'jiejie', 'bear']
+		audios: ['ruirui', 'jiejie', 'xiongxiong']
 	},
-	{ story: 'robot', title: 'Awaken', subtitle: 'Robot said,', audios: ['robot'] },
+	{ story: 'robot', title: 'Awaken', subtitle: 'Robot said,', audios: ['tiantian woke up and'] },
 	{
 		story: 'drivers',
 		title: 'Drivers',
 		subtitle: 'Yumi, Xie & David said,',
-		audios: ['yumi', 'jerry', 'david']
+		audios: ['yumi', 'xiexie', 'david']
 	},
-	{ story: 'robot2', title: 'The Rude Robot', subtitle: 'Robot said,', audios: ['robot'] },
+	{
+		story: 'robot2',
+		title: 'The Rude Robot',
+		subtitle: 'Robot said,',
+		audios: ['tiantian rejected the request and']
+	},
 	{ story: 'robot3', title: "Princess' New Speaker", subtitle: 'Robot said,', audios: ['robot'] },
-	{ story: 'dragon1', title: 'Face Your Fear', subtitle: 'Dragon said,', audios: [] },
-	{ story: 'dragon2', title: 'Victory', subtitle: 'Dragon said,', audios: [] },
-	{ story: 'ending', title: 'Happy Ending', subtitle: 'They said,', audios: [] }
+	{ story: 'dragon1', title: 'Face Your Fear', subtitle: 'Dragon said,', audios: ['dragon'] },
+	{
+		story: 'dragon2',
+		title: 'Victory',
+		subtitle: 'Dragon said,',
+		audios: ['dragon got defeated and']
+	},
+	{
+		story: 'ending',
+		title: 'Happy Ending',
+		subtitle: 'They said,',
+		audios: ['mya wish', 'jiejie wish']
+	}
 
 	// Add more chapters as needed
 ];
